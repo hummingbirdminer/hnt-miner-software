5830a93a01bf6a0aea596cc27b360aff5b50191a
hummingbird <hummingbird@outlook.com>
2021-09-24 11:01:39 +0800

    add miner script

---
+++ hummingbird_miner.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+VERSION=0.1
+SELF_NAME=`basename "$0"`
+
+function startHummingbirdMiner() {
+  echo "Start hummingbird miner"
+  docker-compose up
+}
+
+function stopHummingbirdMiner() {
+  echo "Stop hummingbird miner"
+  docker-compose down
+}
+
+function checkOriginUpdate() {
+  SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)
+
+  git fetch
+
+  HEADHASH=$(git rev-parse HEAD)
+  UPSTREAMHASH=$(git rev-parse main@{upstream})
+
+  if [ "$HEADHASH" != "$UPSTREAMHASH" ]
+  then
+  # stop docker-compose first
+    echo "Do self update"
+    stopHummingbirdMiner
+    git pull
+    chmod +x ${SELF_NAME}
+    exec ${SELF_NAME}
+  fi
+}
+echo ${SELF_NAME}
+checkOriginUpdate
+startHummingbirdMiner
